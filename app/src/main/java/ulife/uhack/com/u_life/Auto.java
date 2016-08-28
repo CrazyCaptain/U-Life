8ec054d585d9b1b260a699049177f53900fe85dc
Reiner <reins@thereinsmeister.com>
2016-08-28 11:06:25 +0800

    Attempted Beautification

--- app/src/main/java/ulife/uhack/com/u_life/Auto.java
+++ app/src/main/java/ulife/uhack/com/u_life/Auto.java
@@ -3,6 +3,8 @@ package ulife.uhack.com.u_life;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Auto extends Activity {
 
@@ -10,5 +12,14 @@ public class Auto extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto);
+
+        Spinner spnBrand = (Spinner) findViewById(R.id.spnBrand);
+        ArrayAdapter<String> adapteBrand = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.carbrand_arrays));
+        spnBrand.setAdapter(adapteBrand);
+
+        Spinner spnAutoYear = (Spinner) findViewById(R.id.spnAutoYear);
+        ArrayAdapter<String> adapterAutoYear = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.years_arrays_auto));
+        spnAutoYear.setAdapter(adapterAutoYear);
+
     }
 }
--- app/src/main/java/ulife/uhack/com/u_life/Education.java
+++ app/src/main/java/ulife/uhack/com/u_life/Education.java
@@ -3,16 +3,23 @@ package ulife.uhack.com.u_life;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Education extends Activity {
+public class Education extends Activity
+{
+
+    TextView txtComputation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
 
+
+        txtComputation = (TextView) findViewById(R.id.txtComputation);
 
         Spinner spnMonth = (Spinner) findViewById(R.id.spnMonth);
         ArrayAdapter<String> adapterMonth = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.month_arrays));
@@ -23,4 +30,10 @@ public class Education extends Activity {
         ArrayAdapter<String> adapterYear = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.years_arrays));
         spnYear.setAdapter(adapterYear);
     }
+
+    public void doComputation(View v)
+    {
+        
+    }
+
 }
